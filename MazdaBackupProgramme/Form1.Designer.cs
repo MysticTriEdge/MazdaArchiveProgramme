1ffbc34439e774a755900c785d3e896195b09e96
Burrows Digital Production <burrowsdigitalproduction@gmail.com>
2017-07-06 14:43:59 +0100

    First push. So far: Backs up directories and files from test folders Timer set to 9pm each night Minimizes to system try and continues to run (not tested)

---
+++ MazdaBackupProgramme/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace MazdaBackupProgramme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblNow = new System.Windows.Forms.Label();
+            this.lblRunTime = new System.Windows.Forms.Label();
+            this.btnRunManual = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Current Time:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Programme Will Run At:";
+            // 
+            // lblNow
+            // 
+            this.lblNow.AutoSize = true;
+            this.lblNow.Location = new System.Drawing.Point(185, 58);
+            this.lblNow.Name = "lblNow";
+            this.lblNow.Size = new System.Drawing.Size(49, 13);
+            this.lblNow.TabIndex = 2;
+            this.lblNow.Text = "00:00:00";
+            // 
+            // lblRunTime
+            // 
+            this.lblRunTime.AutoSize = true;
+            this.lblRunTime.Location = new System.Drawing.Point(185, 100);
+            this.lblRunTime.Name = "lblRunTime";
+            this.lblRunTime.Size = new System.Drawing.Size(49, 13);
+            this.lblRunTime.TabIndex = 3;
+            this.lblRunTime.Text = "99:99:99";
+            // 
+            // btnRunManual
+            // 
+            this.btnRunManual.Location = new System.Drawing.Point(344, 267);
+            this.btnRunManual.Name = "btnRunManual";
+            this.btnRunManual.Size = new System.Drawing.Size(75, 23);
+            this.btnRunManual.TabIndex = 4;
+            this.btnRunManual.Text = "Manual Run";
+            this.btnRunManual.UseVisualStyleBackColor = true;
+            this.btnRunManual.Click += new System.EventHandler(this.btnRunManual_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.Click += new System.EventHandler(this.notifyIcon1_Click);
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info; //Shows the info icon so the user doesn't thing there is an error.
+            this.notifyIcon1.BalloonTipText = "Timer Will continue to run";
+            this.notifyIcon1.BalloonTipTitle = "Mazda Backup Programme Minimized";
+            this.notifyIcon1.Text = "Mazda Backup Programme";
+            this.notifyIcon1.Icon = Properties.Resources.Tray;
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 350);
+            this.Controls.Add(this.btnRunManual);
+            this.Controls.Add(this.lblRunTime);
+            this.Controls.Add(this.lblNow);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Mazda Backup Programme";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblNow;
+        private System.Windows.Forms.Label lblRunTime;
+        private System.Windows.Forms.Button btnRunManual;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ MazdaBackupProgramme/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MazdaBackupProgramme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = (1000) * (1);
+            timer1.Start();
+            
+        }
+
+        private void btnRunManual_Click(object sender, EventArgs e)
+        {
+            int hourNow = DateTime.Now.Hour;
+
+            int minNow = DateTime.Now.Minute;
+
+            runBackup();
+        }
+
+
+        public void runBackup()
+        {
+            int dayFrom = DateTime.Now.Day;
+            int monthFrom = DateTime.Now.Month - 3;
+            int yearFrom = DateTime.Now.Year;
+
+            string stagingFolder = @"C:\Users\grimwoodb\Desktop\Test Folder";
+            string archiveFolder = @"C:\Users\grimwoodb\Desktop\Test Folder 2";
+
+            List<string> movedFolders = new List<string>();
+
+            foreach (var dirflder in Directory.GetDirectories(stagingFolder))
+            {
+                string dirflderName = dirflder.Remove(0, stagingFolder.Length);
+
+                foreach (var flder in Directory.GetDirectories(stagingFolder + dirflderName))
+                {
+                    var fldName = new DirectoryInfo(flder).Name;
+                    int fldMonth = Convert.ToInt32(fldName.Substring(3, 2));
+                    int fldYear = Convert.ToInt32(fldName.Substring(6, 4));
+
+
+                    if (fldMonth <= monthFrom && fldYear == yearFrom)
+                    {
+                        movedFolders.Add(flder);
+                        if (!Directory.Exists(archiveFolder + dirflderName + "\\" + fldName))
+                        {
+                            Directory.CreateDirectory(archiveFolder + dirflderName);
+                            Directory.Move(flder, archiveFolder + dirflderName + "\\" + fldName);
+                        }
+                        else if (!Directory.Exists(archiveFolder + dirflderName + "\\" + fldName))
+                        {
+                            Directory.Move(flder, archiveFolder + dirflderName + "\\" + fldName + "\\" + "Copy");
+                        }
+                    }
+                    else if (fldYear < yearFrom)
+                    {
+                        movedFolders.Add(flder);
+                        if (!Directory.Exists(archiveFolder + dirflderName + "\\" + fldName))
+                        {
+                            Directory.CreateDirectory(archiveFolder + dirflderName);
+                            Directory.Move(flder, archiveFolder + dirflderName + "\\" + fldName);
+                        }
+                        else if (!Directory.Exists(archiveFolder + dirflderName + "\\" + fldName))
+                        {
+                            Directory.Move(flder, archiveFolder + dirflderName + "\\" + fldName + "\\" + "Copy");
+                        }
+                    }
+
+                }
+
+            }
+
+            string logfilePath = @"C:\Users\grimwoodb\Desktop\Test Folder 2\archieved.txt";
+            movedFolders.Insert(0, DateTime.Now.ToString());
+
+            if (File.Exists(logfilePath))
+            {
+                
+                foreach (string line in movedFolders)
+                {
+                    TextWriter tsw = new StreamWriter(logfilePath, true);
+                    tsw.WriteLine(line);
+                    tsw.Close();
+                }
+                
+            }
+            else {
+                File.WriteAllLines(logfilePath, movedFolders);
+            }
+            
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var runTime = "21:00";
+            lblRunTime.Text = runTime.ToString();
+
+            int hourNow = DateTime.Now.Hour;
+
+            int minNow = DateTime.Now.Minute;
+
+            string timeNow = hourNow + ":" + minNow;
+
+            lblNow.Text = timeNow;
+
+            if (hourNow == 21 && minNow == 00)
+            {
+                runBackup();
+            }
+
+        }
+
+        private void notifyIcon1_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+            notifyIcon1.Visible = false;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                notifyIcon1.Visible = true;
+                notifyIcon1.ShowBalloonTip(3000);
+                this.ShowInTaskbar = false;
+            }
+        }
+    }
+}
